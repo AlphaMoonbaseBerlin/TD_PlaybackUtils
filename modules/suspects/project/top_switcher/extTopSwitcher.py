230ac4e72cde30aea0a003ee187eccb79fcd300a
AlphaMoonbaseBerlin <kontakt@alphamoonbase.de>
2023-10-17 11:20:06 +0200

    tOPsWITCHER UPDATES0.10

--- modules/suspects/project/top_switcher/extTopSwitcher.py
+++ modules/suspects/project/top_switcher/extTopSwitcher.py
@@ -5,62 +5,56 @@ Saveorigin : Project.toe
 Saveversion : 2022.32660
 Info Header End'''
 
+from typing import List
 
-
-import math
 class extTopSwitcher:
 	"""
 	extTopSwitcher description
 	"""
 	def __init__(self, ownerComp):
 		# The component to which this extension is attached
-		self.ownerComp = ownerComp
-		self.callback = self.ownerComp.op('callbackManager')
-		self.standby_selectors = []
-		self.active_selectors = []
-		for selector in self.ownerComp.findChildren( name = "selector*" ) : self.Unload( selector, do_callback = False)
-		self.tweenParameters = [
+		self.ownerComp:COMP 	= ownerComp
+		self.callback:COMP 		= self.ownerComp.op('callbackManager')
+		self.standbySelectors:List[COMP] 	= []
+		self.activeSelectors :List[COMP] 	= []
+
+		self.parameterNames:List[str] = [
 			"alpha", "offsetx", "offsety", "scalew", "scaleh", "rotation", "state"
 		]
 
+		for selector in self.ownerComp.findChildren( name = "selector*" ) : self._unload( selector, do_callback = False)
+		
+		#backwardscompatible
+		self.Select_Top 	= self.SelectTop
+		self.Select_Input 	= self.SelectInput
+
 	@property
-	def tweener(self):
+	def tweener(self) -> "COMP":
 		return self.ownerComp.par.Tweener.eval()
 
-	def Unload(self, selector, do_callback = True):
-		target_top = selector.par.Top.eval()
+	def _unload(self, selector:COMP, do_callback = True):
+		target_top:TOP 		= selector.par.Top.eval()
 		selector.par.Top 	= ''
 		selector.par.Alpha 	= 0
-		selector.par.State  = 0
-		self.standby_selectors.append( selector )
+		self.standbySelectors.append( selector )
 		try:
-			self.active_selectors.remove( selector )
+			self.activeSelectors.remove( selector )
 		except:
 			pass
-		if do_callback: self.callback.Do_Callback( "OnUnload", target_top )
+		if do_callback: self.callback.Do_Callback( "onUnload", target_top, self.ownerComp )
 
-	def fade_to(self, selector, target, time):
-		for tweenParameter in self.tweenParameters:
-			targetParameter = selector.par[ tweenParameter.capitalize() ]
-			sourceParameter = self.ownerComp.par[f"{target}{tweenParameter}"]
-			if time:
-				difference = math.abs( targetParameter.eval() - sourceParameter.eval() )
-				self.tweener.RelativeTween( 
-						targetParameter, 
-						sourceParameter.eval(), 
-						time/difference)
-			else:
-				targetParameter = selector.par[ tweenParameter.capitalize() ]
-				sourceParameter = self.ownerComp.par[f"{target}{tweenParameter}"]
-				targetParameter.val = sourceParameter.eval()
-			
-	def Select_Top(self, top, time):
-		if not self.standby_selectors: return
+	def _fadeTo(self, selector:COMP, target:str, time:float):
+		for parameterName in self.parameterNames:
+			self.tweener.AbsoluteTween( selector.par[ parameterName.capitalize() ], self.ownerComp.par[f"{target}{parameterName}"].eval(), time)
 
-		for active_selector in self.active_selectors: self.fade_to( active_selector, "End", time)
+	def SelectTop(self, top:TOP, time:float):
+		if not self.standbySelectors: return
 
-		selector = self.standby_selectors.pop()
-		self.active_selectors.append( selector )
+		for active_selector in self.activeSelectors: 
+			if active_selector.par.State.eval(): self._fadeTo( active_selector, "End", time)
+
+		selector = self.standbySelectors.pop()
+		self.activeSelectors.append( selector )
 
 		selectorTop = selector.op("video_out")
 		selectorTopPath = self.ownerComp.op("comp1").relativePath( selectorTop )
@@ -71,10 +65,10 @@ class extTopSwitcher:
 		self.ownerComp.op("selectedTops").appendRow( selectorTopPath )
 
 		selector.par.Top = top
-		self.fade_to( selector, "Start", 0)
-		self.fade_to( selector, "Target", time)
-		self.callback.Execute( "OnLoad" )( top )
+		self._fadeTo( selector, "Start", 0)
+		self._fadeTo( selector, "Target", time)
+		self.callback.Do_Callback( "onLoad", top, self.ownerComp )
 
-	def Select_Input(self, index, time):
+	def SelectInput(self, index:int, time:float):
 		input = self.ownerComp.op(f"input{index}")
-		self.Select_Top( input, time )
+		self.SelectTop( input, time )
