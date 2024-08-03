8e72f1fc1604a5fd585800ed46377a476d1a7500
AlphaMoonbaseBerlin <kontakt@alphamoonbase.de>
2024-08-03 23:49:20 +0200

    topswitcher proper overlay. store data in OP world!

--- modules/suspects/project/top_switcher/extTopSwitcher.py
+++ modules/suspects/project/top_switcher/extTopSwitcher.py
@@ -30,12 +30,22 @@ class extTopSwitcher:
 		return self.ownerComp.par.Tweener.eval()
 
 	@property
+	def selectors(self):
+		return self.ownerComp.findChildren(name = "selector[1-99]", depth = 1)
+
+	@property
+	def sortedSelectors(self):
+		return sorted( 
+			self.selectors, 
+			key = lambda selector : selector.fetch("_selectionTimestamp", 0))
+
+	@property
 	def availableSelectors(self):
-		return [ operator for operator in self.ownerComp.findChildren(name = "selector[1-99]", depth = 1) if not operator.par.State.eval()]
+		return [ operator for operator in self.selectors if not operator.par.State.eval()]
 
 	@property
 	def activeSelectors(self):
-		return [ operator for operator in self.ownerComp.findChildren(name = "selector[1-99]", depth = 1) if operator.par.State.eval()]
+		return [ operator for operator in self.selectors if operator.par.State.eval()]
 
 
 	def _unload(self, selector:COMP, do_callback = True):
@@ -46,10 +56,12 @@ class extTopSwitcher:
 		if do_callback: self.callback.Do_Callback( "onUnload", target_top, self.ownerComp )
 
 	def _fadeTo(self, selector:COMP, target:str, time:float):
-
+		# currentStateValue 	= selector.par.State.eval()
+		# targetStateVaue 	= self.ownerComp.par[f"{target}state"].eval()
+		# timeAdjustment 		= abs( currentStateValue - targetStateVaue)
+		timeAdjustment = 1
 		for parameterName in self._absolueParameterNames:
-			timeAdjustment = selector.par.State.eval()
-			timeAdjustment = 1
+			
 
 			self.tweener.AbsoluteTween( 
 				selector.par[ parameterName.capitalize() ], 
@@ -62,13 +74,14 @@ class extTopSwitcher:
 			self._fadeTo( fadeOutSelector, "End", time)
 
 		try:
-			selector = self.availableSelectors.pop()
+			fadeInSelector = self.availableSelectors.pop()
 		except IndexError:
 			return False
 
-		selector.par.Top = top
-		self._fadeTo( selector, "Start", 0)
-		self._fadeTo( selector, "Target", time)
+		fadeInSelector.par.Top = top
+		fadeInSelector.store( "_selectionTimestamp", absTime.frame )
+		self._fadeTo( fadeInSelector, "Start", 0)
+		self._fadeTo( fadeInSelector, "Target", time)
 		self.callback.Do_Callback( "onLoad", top, self.ownerComp )
 		return True
 	
