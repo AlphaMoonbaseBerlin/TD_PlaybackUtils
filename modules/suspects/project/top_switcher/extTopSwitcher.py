71e82d753fc9a0d2d8dec517537348b7fa6b9fa7
AlphaMoonbaseBerlin <kontakt@alphamoonbase.de>
2024-08-03 23:36:07 +0200

    TopSwitcher State is no longer stored in extension but derived from actual state. Phase adjustements.

--- modules/suspects/project/top_switcher/extTopSwitcher.py
+++ modules/suspects/project/top_switcher/extTopSwitcher.py
@@ -3,7 +3,7 @@
 Name : extTopSwitcher
 Author : Wieland@AMB-ZEPH15
 Saveorigin : Project.toe
-Saveversion : 2022.32660
+Saveversion : 2022.35320
 Info Header End'''
 
 from typing import List
@@ -16,14 +16,10 @@ class extTopSwitcher:
 		# The component to which this extension is attached
 		self.ownerComp:COMP 	= ownerComp
 		self.callback:COMP 		= self.ownerComp.op('callbackManager')
-		self.standbySelectors:List[COMP] 	= []
-		self.activeSelectors :List[COMP] 	= []
 
-		self.parameterNames:List[str] = [
+		self._absolueParameterNames:List[str] = [
 			"alpha", "offsetx", "offsety", "scalew", "scaleh", "rotation", "state"
 		]
-
-		for selector in self.ownerComp.findChildren( name = "selector*" ) : self._unload( selector, do_callback = False)
 		
 		#backwardscompatible
 		self.Select_Top 	= self.SelectTop
@@ -33,44 +29,49 @@ class extTopSwitcher:
 	def tweener(self) -> "COMP":
 		return self.ownerComp.par.Tweener.eval()
 
+	@property
+	def availableSelectors(self):
+		return [ operator for operator in self.ownerComp.findChildren(name = "selector[1-99]", depth = 1) if not operator.par.State.eval()]
+
+	@property
+	def activeSelectors(self):
+		return [ operator for operator in self.ownerComp.findChildren(name = "selector[1-99]", depth = 1) if operator.par.State.eval()]
+
+
 	def _unload(self, selector:COMP, do_callback = True):
 		target_top:TOP 		= selector.par.Top.eval()
 		selector.par.Top.val 	= ''
 		selector.par.Alpha.val 	= 0
 		selector.par.State.val 	= 0
-		self.standbySelectors.append( selector )
-		try:
-			self.activeSelectors.remove( selector )
-		except:
-			pass
 		if do_callback: self.callback.Do_Callback( "onUnload", target_top, self.ownerComp )
 
 	def _fadeTo(self, selector:COMP, target:str, time:float):
-		for parameterName in self.parameterNames:
-			self.tweener.AbsoluteTween( selector.par[ parameterName.capitalize() ], self.ownerComp.par[f"{target}{parameterName}"].eval(), time)
+
+		for parameterName in self._absolueParameterNames:
+			timeAdjustment = selector.par.State.eval()
+			timeAdjustment = 1
+
+			self.tweener.AbsoluteTween( 
+				selector.par[ parameterName.capitalize() ], 
+				self.ownerComp.par[f"{target}{parameterName}"].eval(), 
+				time * timeAdjustment
+			)
 
 	def SelectTop(self, top:TOP, time:float, force:bool = False):
-		if not self.standbySelectors: return
-		if (not force) and (top in [selector.par.Top.eval() for selector in self.activeSelectors]): return
-		for active_selector in self.activeSelectors: 
-			if active_selector.par.State.eval(): self._fadeTo( active_selector, "End", time)
+		for fadeOutSelector in self.activeSelectors:
+			self._fadeTo( fadeOutSelector, "End", time)
 
-		selector = self.standbySelectors.pop()
-		self.activeSelectors.append( selector )
-
-		selectorTop = selector.op("video_out")
-		selectorTopPath = self.ownerComp.op("comp1").relativePath( selectorTop )
 		try:
-			self.ownerComp.op("selectedTops").deleteRow( selectorTopPath )
-		except tdError:
-			pass
-		self.ownerComp.op("selectedTops").appendRow( selectorTopPath )
+			selector = self.availableSelectors.pop()
+		except IndexError:
+			return False
 
 		selector.par.Top = top
 		self._fadeTo( selector, "Start", 0)
 		self._fadeTo( selector, "Target", time)
 		self.callback.Do_Callback( "onLoad", top, self.ownerComp )
-
+		return True
+	
 	def SelectInput(self, index:int, time:float):
 		input = self.ownerComp.op(f"input{index}")
 		self.SelectTop( input, time )
